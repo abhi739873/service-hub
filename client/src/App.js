24d4a969adedc4c27e35c382a25deba0be09da67
abhishek <abhishek123chs@gmail.com>
2024-07-05 00:49:58 +0530

    change link

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import {useNavigate} from "react-router-dom";
 import {Toaster} from "react-hot-toast";
 import BookingComplete from "./components/bookingcomplete";
 const AuthContext=React.createContext();
-export const backendUrl="https://servicehub2.onrender.com";
+export const backendUrl="https://service-hub-a3g9.onrender.com";
 function App() {
   return (
     <Router>
